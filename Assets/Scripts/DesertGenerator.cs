2985e9ee78a9c62e94815f1e4946e16fc133072f
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-10-05 19:54:50 +0200

    Add stuff

---
+++ Assets/Scripts/DesertGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesertGenerator : MonoBehaviour
+{
+    public GameObject sandCudePrefab;
+    public int size = 10;
+    public int depth = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Generate()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int k = 0; k < depth; k++)
+                {
+                    Instantiate(sandCudePrefab, new Vector3(i, k - 5, j), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotationSpeed = 1.0f;
+
+    private Rigidbody _rigidbody;
+    private Quaternion _cameraRotation;
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+
+        if (Camera.main != null)
+            _cameraRotation = Quaternion.AngleAxis(Camera.main.transform.rotation.eulerAngles.y, Vector3.up);
+    }
+
+    private void Update()
+    {
+        var direction = _cameraRotation *
+                        new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+
+        var velocity = direction * speed * Time.deltaTime;
+
+        _rigidbody.velocity = velocity;
+        
+        _animator.SetFloat("Velocity", velocity.magnitude);
+
+        if (direction != Vector3.zero)
+        {
+            var targetRotation = Quaternion.LookRotation(direction);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
