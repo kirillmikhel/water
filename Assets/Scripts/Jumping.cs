a978b47be7b24d05bbbef19f5c34b1c423b135b4
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-10-06 20:20:15 +0200

    Add jumping and a chest

---
+++ Assets/Scripts/Jumping.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+    public float force = 10f;
+
+    private bool _isLanded = true;
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Jump") == 1 && _isLanded)
+        {
+            StartCoroutine(Jump());
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, force, _rigidbody.velocity.z);
+
+        _isLanded = false;
+
+        yield return new WaitForSeconds(1f);
+
+        _isLanded = true;
+    }
+}
