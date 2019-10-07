00d7116df37cc3b30444dc61741522b87a00f6d2
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-10-07 20:54:09 +0200

    Update UI

---
+++ Assets/Scripts/InventoryContent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryContent : MonoBehaviour
+{
+    private Text _text;
+    private Inventory _inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        _text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var text = "";
+
+        foreach (var item in _inventory.items)
+        {
+            text +=  item.ToString();
+            
+            switch (item)
+            {
+                case ItemType.Artifact:
+                    text += "  (Hold Tab)";
+                    break;
+                case ItemType.Shovel:
+                    text += "  (Z)";
+                    break;
+            }
+
+            text += "\n";
+        }
+
+        _text.text = text;
+    }
+}
